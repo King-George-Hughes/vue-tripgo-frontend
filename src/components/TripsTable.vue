431854ad97dd085407fcf4b61c96e69851700e23
King-George-Hughes <hughesgeorgeking@gmail.com>
2024-09-01 21:28:17 +0000

    more changes

--- src/components/TripsTable.vue
+++ src/components/TripsTable.vue
@@ -46,7 +46,7 @@ defineProps({
                 <tr>
                   <th
                     scope="col"
-                    class="px-6 py-3 text-start border-s border-gray-200 dark:border-neutral-700"
+                    class="hidden lg:table-cell px-6 py-3 text-start border-s border-gray-200 dark:border-neutral-700"
                   >
                     <span
                       class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200"
@@ -55,7 +55,7 @@ defineProps({
                     </span>
                   </th>
 
-                  <th scope="col" class="px-6 py-3 text-start">
+                  <th scope="col" class="hidden lg:table-cell px-6 py-3 text-start">
                     <span
                       class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200"
                     >
@@ -63,7 +63,7 @@ defineProps({
                     </span>
                   </th>
 
-                  <th scope="col" class="px-6 py-3 text-start">
+                  <th scope="col" class="hidden lg:table-cell px-6 py-3 text-start">
                     <span
                       class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200"
                     >
@@ -71,7 +71,7 @@ defineProps({
                     </span>
                   </th>
 
-                  <th scope="col" class="px-6 py-3 text-start">
+                  <th scope="col" class="hidden lg:table-cell px-6 py-3 text-start">
                     <span
                       class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200"
                     >
@@ -79,7 +79,7 @@ defineProps({
                     </span>
                   </th>
 
-                  <th scope="col" class="px-6 py-3 text-start">
+                  <th scope="col" class="hidden lg:table-cell px-6 py-3 text-start">
                     <span
                       class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200"
                     >
@@ -87,18 +87,26 @@ defineProps({
                     </span>
                   </th>
 
-                  <th scope="col" class="px-6 py-3 text-start">
+                  <th scope="col" class="hidden lg:table-cell px-6 py-3 text-start">
                     <span
                       class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200"
                     >
+                    </span>
+                  </th>
+
+                  <th scope="col" class="lg:hidden px-6 py-3 text-start">
+                    <span
+                      class="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200"
+                    >
+                      Schedules
                     </span>
                   </th>
                 </tr>
               </thead>
 
               <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
-                <tr v-for="schedule in schedules" :key="schedule.id">
-                  <td class="h-px w-auto whitespace-nowrap">
+                <tr v-for="(schedule, index) in schedules" :key="schedule.id">
+                  <td class="hidden lg:table-cell h-px w-auto whitespace-nowrap">
                     <div class="px-6 py-2 flex items-center gap-x-3">
                       <div class="flex items-start flex-col gap-2" href="#">
                         <p class="capitalize font-semibold pl-1">
@@ -111,7 +119,7 @@ defineProps({
                     </div>
                   </td>
 
-                  <td class="h-px w-auto whitespace-nowrap">
+                  <td class="hidden lg:table-cell h-px w-auto whitespace-nowrap">
                     <div class="px-6 py-2 flex items-start gap-2 flex-col">
                       <span class="text-gray-800 text-sm"
                         ><Tag value="From:" severity="info" style="width: 50px; padding: 2px"></Tag>
@@ -130,7 +138,7 @@ defineProps({
                     </div>
                   </td>
 
-                  <td class="h-px w-auto whitespace-nowrap">
+                  <td class="hidden lg:table-cell h-px w-auto whitespace-nowrap">
                     <div class="px-6 py-2 flex flex-col items-start gap-2">
                       <div class="text-sm text-gray-800 dark:text-neutral-200">
                         {{ formatDayOfWeek(schedule.departureTime) }} -
@@ -144,89 +152,131 @@ defineProps({
                       </div>
                     </div>
                   </td>
-                  <td class="h-px w-auto whitespace-nowrap">
+
+                  <td class="hidden lg:table-cell h-px w-auto whitespace-nowrap">
                     <div class="px-6 py-2">
                       <span class="text-sm text-gray-600 lowercase">
                         <Tag severity="success" :value="schedule.bus.status"></Tag>
                       </span>
                     </div>
                   </td>
-                  <td class="h-px w-auto whitespace-nowrap">
+
+                  <td class="hidden lg:table-cell h-px w-auto whitespace-nowrap">
                     <div class="px-6 py-2">
                       <span class="text-sm text-gray-800 dark:text-neutral-200"
                         >$ {{ schedule.fare }}</span
                       >
                     </div>
                   </td>
-                  <td class="h-px w-auto whitespace-nowrap">
+
+                  <td class="hidden lg:table-cell h-px w-auto whitespace-nowrap">
                     <div class="px-6 py-2 inline-flex items-center gap-3">
                       <!-- Testing Booking form -->
                       <BookingForm :schedule="schedule" />
+                    </div>
+                  </td>
+
+                  <!-- Mobile View -->
+                  <td class="lg:hidden h-px w-auto whitespace-nowrap">
+                    <div class="px-6 py-3 flex items-start gap-2 flex-col">
+                      <div class="w-full flex justify-between">
+                        <div class="flex items-start flex-col gap-2" href="#">
+                          <p class="capitalize font-semibold pl-1">
+                            {{ schedule.bus.model }}
+                          </p>
+                          <p>
+                            <Tag severity="secondary" :value="schedule.bus.plateNumber"></Tag>
+                          </p>
+                        </div>
+
+                        <div
+                          class="font-semibold text-xl bg-black/70 w-8 h-8 rounded-full flex items-center justify-center text-gray-50"
+                        >
+                          {{ index + 1 }}
+                        </div>
+                      </div>
+
+                      <div class="w-full flex justify-between">
+                        <div class="flex items-start gap-2 flex-col">
+                          <p class="underline font-semibold">Journey</p>
+                          <span class="text-gray-800 text-sm"
+                            ><Tag
+                              value="From:"
+                              severity="info"
+                              style="width: 50px; padding: 2px"
+                            ></Tag>
+                            {{ schedule.origin.name }} ({{ schedule.origin.station }})
+                          </span>
+
+                          <span class="text-gray-800">
+                            <Tag
+                              value="To:"
+                              severity="info"
+                              style="width: 50px; padding: 2px"
+                            ></Tag>
+                            {{ schedule.destination.name }} ({{
+                              schedule.destination.station
+                            }})</span
+                          >
+
+                          <span class="text-gray-800">
+                            <Tag
+                              value="Cap:"
+                              severity="warn"
+                              style="width: 50px; padding: 2px"
+                            ></Tag>
+                            {{ schedule.bus.seatCapacity }}</span
+                          >
+                        </div>
+
+                        <div>
+                          <p class="underline font-semibold mb-1 text-end">Amount</p>
+                          <p class="text-sm text-gray-800 dark:text-neutral-200">
+                            $ {{ schedule.fare }}
+                          </p>
+                        </div>
+                      </div>
+
+                      <div class="w-full flex justify-between mt-1">
+                        <div class="py-2 flex flex-col items-start gap-2">
+                          <p class="underline font-semibold">Schedule</p>
+                          <div class="text-sm text-gray-800 dark:text-neutral-200">
+                            {{ formatDayOfWeek(schedule.departureTime) }} -
+                            <span class="font-semibold">{{
+                              formatDate(schedule.departureTime)
+                            }}</span>
+                          </div>
+
+                          <div class="text-sm py-1">
+                            <Tag
+                              severity="secondary"
+                              :value="formatTime(schedule.departureTime)"
+                            ></Tag>
+                            -
+                            <Tag
+                              severity="secondary"
+                              :value="formatTime(schedule.arrivalTime)"
+                            ></Tag>
+                          </div>
+                        </div>
+
+                        <div>
+                          <p class="underline font-semibold text-end mb-2">Status</p>
+                          <p class="text-sm text-gray-600 lowercase">
+                            <Tag severity="success" :value="schedule.bus.status"></Tag>
+                          </p>
+                        </div>
+                      </div>
+
+                      <div class="inline-flex items-center gap-3">
+                        <BookingForm :schedule="schedule" />
+                      </div>
                     </div>
                   </td>
                 </tr>
               </tbody>
             </table>
             <!-- End Table -->
-
-            <!-- Footer -->
-            <div
-              class="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-neutral-700"
-            >
-              <div>
-                <p class="text-sm text-gray-600 dark:text-neutral-400">
-                  <span class="font-semibold text-gray-800 dark:text-neutral-200">9</span>
-                  results
-                </p>
-              </div>
-
-              <div>
-                <div class="inline-flex gap-x-2">
-                  <button
-                    type="button"
-                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
-                  >
-                    <svg
-                      class="flex-shrink-0 size-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="m15 18-6-6 6-6" />
-                    </svg>
-                    Prev
-                  </button>
-
-                  <button
-                    type="button"
-                    class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
-                  >
-                    Next
-                    <svg
-                      class="flex-shrink-0 size-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path d="m9 18 6-6-6-6" />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <!-- End Footer -->
           </div>
         </div>
       </div>
